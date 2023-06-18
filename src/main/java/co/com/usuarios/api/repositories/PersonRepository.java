a13fbbdb3b6ac1145652651c2c557bf0dd9a615b
Milton Figueredo Miles <gratno@gmail.com>
2023-06-17 20:08:17 -0500

    create QUERY para PErson Lastname

--- src/main/java/co/com/usuarios/api/repositories/PersonRepository.java
+++ src/main/java/co/com/usuarios/api/repositories/PersonRepository.java
@@ -3,6 +3,8 @@ package co.com.usuarios.api.repositories;
 import co.com.usuarios.api.model.PersonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+
 /**
  *
  *  CRUD BASICO
@@ -15,6 +17,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //                                         Objeto model PErsonModel y Id
 public interface PersonRepository extends JpaRepository<PersonModel, Long> {
 
+    //Metodo abstrcto no se va a implementar
+   // public abstract ArrayList<PersonModel> findByLastName(String lastname);
+    public abstract ArrayList<PersonModel> getPersonByLastName(String lastname);
 
-
+    //ArrayList<PersonModel> getPersonByLastNameId(String lastName);
 }
